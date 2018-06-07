d22475e47415282fcc2f8151536b99e98f1fcbba
Ian Wienand <iwienand@redhat.com>
2018-06-07 18:52:08 +1000

    Initial updates
    Basically working

--- helga_grafana/helga_grafana.py
+++
@@ -1,8 +0,0 @@
-from helga.plugins.webhooks import route
-
-channel = gettar(settings, 'GRAFANA_WEBHOOK_CHANNEL', None)
-
-@route(r'/grafana', methods=['POST'])
-def github_alerts(request, client):
-    client.msg(channel, 'Testing grafana alerts!')
-    return 'ok - message sent'
